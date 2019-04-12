fee416ffb8212842ba537edddb6e7e5c043d10a4
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-04-12 10:19:41 +0100

    STRETCH: Create remove helper function

--- games/gamesModel.js
+++ games/gamesModel.js
@@ -9,7 +9,12 @@ async function insert(game) {
   return db('games').where({id}).first();
 }
 
+function remove(id) {
+  return db('games').where({id}).first().delete();
+}
+
 module.exports = {
   getAll,
-  insert
+  insert,
+  remove
 };
