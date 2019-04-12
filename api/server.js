39575c66a2d41e3722353135c5709a4b7a9ea107
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-04-12 09:42:44 +0100

    MVP: Create endpoints for GET and POST

--- api/server.js
+++ api/server.js
@@ -1,6 +1,37 @@
 const express = require('express');
 
+const Games = require('../games/gamesModel.js');
+const db = require('../data/dbConfig');
+
 const server = express();
 server.use(express.json());
+
+server.get('/', async (req, res) => {
+  res.status(200).json({ api: "up" });
+});
+
+server.get('/games', async (req, res) => {
+  const rows = await Games.getAll();
+
+  res.status(200).json(rows);
+});
+
+server.post('/games', (req, res) => {
+  const { title, genre, releaseYear } = req.body;
+  if (!title || !genre || !releaseYear) {
+    res.status(422).json({ errorMessage: 'Please provide required details.' });
+  } else {
+    db('games').insert({ title, genre, releaseYear })
+        .then(arrayOfIds => {
+          return db('games').where({ id: arrayOfIds[0] })
+        })
+        .then(arrayOfGames => {
+          res.status(201).json({ ...arrayOfGames[0] });
+        })
+        .catch(error => {
+          res.status(500).json({ errorMessage: 'The game could not be added.' });
+        });
+  }
+});
 
 module.exports = server;
